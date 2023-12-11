239a67181900b10654129f833470f6210de1f053
Divyaranjan Pradhan <divyaranjan619@gmail.com>
2023-12-12 02:32:23 +0530

    Form Validation Update

--- validation.js
+++ validation.js
@@ -1,39 +1,53 @@
 const form = document.getElementById("form");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
+const body = document.querySelector("#main-body");
 var isPassValid = false;
 var isEmailValid = false;
 
 form.addEventListener("submit", (e) => {
-  // to prevent the form from submiting becuase we need to validate before it
+  // to prevent the form from submiting because we need to validate before it
   e.preventDefault();
 
-  validateInput();
+  flag=validateInput();
+    if(flag){
+        email.value='';
+        password.value='';
+        window.location.assign("test.html");
+    }
 });
 
+
+body.addEventListener("click",(e)=>{
+    if(email.value.length != 0  || password.value.length!=0){
+        validateInput();
+    }
+});
+
+
+// used for setting error message
 const setError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
 
-  errorDisplay.innerText = message;
-  inputControl.classList.add("error-border");
-  inputControl.classList.remove("success-border");
+    errorDisplay.innerText = message;
 };
 
+
+// when there's no error it removes the 'error'
 const setSuccess = (element) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector(".error");
 
-  errorDisplay.innerText = '';
-  inputControl.classList.add("success-border");
-  inputControl.classList.remove("error-border");
+    errorDisplay.innerText = '';
 };
+
 
 // this function is used to validate the input
 const validateInput = () => {
     const emailVal = email.value.trim();
     const passwordVal = password.value.trim();
-    const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+    const passRegex = /(.*)/;
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
     // validations for email
@@ -50,27 +64,24 @@ const validateInput = () => {
         setSuccess(email);
     }
 
-
     // validations for password
     if (passwordVal === "") {
         isPassValid = false;
-        setError(password, "Enter Password");
+        setError(password, "Password is required");
     } 
-    else if (!passRegex.test(passwordVal)) {
+    else if (passwordVal.length < 8) {
         isPassValid = false;
-        setError(password, "Password is not valid");
+        setError(password, "Too Short");
     } 
-    else {
+    else if(passRegex.test(passwordVal) && passwordVal.length >= 8) {
         isPassValid = true;
         setSuccess(password);
     }
 
-    //to check if login is successful
+    //to check if login is successful, if successful return true
     if(isEmailValid && isPassValid)
     {
-        email.value='';
-        password.value='';
-        window.location.assign("test.html");
+        return true;
     }
 
 };
